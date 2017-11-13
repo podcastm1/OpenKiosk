1c5eb638bd9a5acc2e8a7f63f315c59242e81966
Laino Kim <podcastm1@gmail.com>
2017-11-14 03:42:43 +0900

    add payment method activity

---
+++ things/src/main/java/ml/laino/com/kingkiosk/IdleActivity.java
@@ -0,0 +1,14 @@
+package ml.laino.com.kingkiosk;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class IdleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idle);
+    }
+
+}
